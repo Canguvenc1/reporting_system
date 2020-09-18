76ec57aae3be59d9e4a673e2e0c9f9e1aefd5f9c
Can Guvenc <canguvenc00@gmail.com>
2020-09-18 16:19:20 -0400

    Log error second commit

--- src/main/java/com/antra/evaluation/reporting_system/endpoint/LogError.java
+++ src/main/java/com/antra/evaluation/reporting_system/endpoint/LogError.java
@@ -12,45 +12,45 @@ public class LogError {
 
     //Checks the description information is involved in the request.
     public String checkRequest(ExcelRequest request){
-        error="NoError";
+        error="";
         if(request.getDescription()==null){
-            error="Your request doesn't have description";
+            error=error+ " Your request doesn't have description";
         }
         //Checks the data information is involved in the request.
         if(request.getData()==null){
-            error="Your request doesn't have any data";
+            error= error+ " Your request doesn't have any data, file didn't created";
         }
         //Checks the header information is involved in the request.
         if(request.getHeaders()==null){
-            error="Your request doesn't have any header";
+            error=error+ " Your request doesn't have any header, file didn't created";
         }
         //Checks the submitter information is involved in the request.
         if(request.getSubmitter()==null){
-            error="Your request doesn't have a submitter information";
+            error=error+ " Your request doesn't have a submitter information";
         }
         return error;
     }
 
     //Checks multi sheet Excel Files request informations.
     public String checkMultiRequest(MultiSheetExcelRequest request){
-        error="NoError";
+        error="";
         if(request.getDescription()==null){
-            error="Your request doesn't have description";
+            error=error+  " Your request doesn't have description";
         }
         //Checks the data information is involved in the request.
         if(request.getData()==null){
-            error="Your request doesn't have any data";
+            error=error+ " Your request doesn't have any data file didn't created";
         }
         //Checks the header information is involved in the request.
         if(request.getHeaders()==null){
-            error="Your request doesn't have any header";
+            error=error+ " Your request doesn't have any header file didn't created";
         }
         //Checks the submitter information is involved in the request.
         if(request.getSubmitter()==null){
-            error="Your request doesn't have a submitter information";
+            error=error+ " Your request doesn't have a submitter information";
         }
         if(request.getSplitBy()==null){
-            error="Your request doesn't have information for split operation";
+            error=error+ " Your request doesn't have information for split operation file didn't created";
         }
         return error;
     }

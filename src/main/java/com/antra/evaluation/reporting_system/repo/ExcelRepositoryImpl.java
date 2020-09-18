5320e8c90f65151e9a8fecafa3e0c3697e4b64a1
Can Guvenc <canguvenc00@gmail.com>
2020-09-18 02:53:24 -0400

    repository files updated

--- src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
+++ src/main/java/com/antra/evaluation/reporting_system/repo/ExcelRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.antra.evaluation.reporting_system.repo;
 import com.antra.evaluation.reporting_system.pojo.report.ExcelFile;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -13,24 +14,58 @@ public class ExcelRepositoryImpl implements ExcelRepository {
 
     Map<String, ExcelFile> excelData = new ConcurrentHashMap<>();
 
+
+    /**
+     *
+     * @param id
+     * @return ExcelFile
+     * Gets the Excel Files by their id's.
+     */
     @Override
     public Optional<ExcelFile> getFileById(String id) {
         return Optional.ofNullable(excelData.get(id));
     }
 
+    /**
+     *
+     * @param file
+     * @return ExcelFile
+     * Saves the Excel Files which is generated.
+     */
     @Override
     public ExcelFile saveFile(ExcelFile file) {
-        return null;
+        excelData.put(file.getFileName(),file);
+        return file;
     }
+
+    /**
+     *
+     * @param id
+     * @return ExcelFile
+     * Removes the Excel Files permanently
+     */
 
     @Override
     public ExcelFile deleteFile(String id) {
-        return null;
+        ExcelFile file=excelData.get(id);
+        excelData.remove(id);
+        return file;
     }
+
+    /**
+     *
+     * @return ExcelFile List
+     * Helps to get the Excel Files
+     */
 
     @Override
     public List<ExcelFile> getFiles() {
-        return null;
+        List<ExcelFile> files=new ArrayList<>();
+        for (Map.Entry<String,ExcelFile> entry : excelData.entrySet()){
+            files.add(entry.getValue());
+        }
+
+        return files;
     }
 }
 
